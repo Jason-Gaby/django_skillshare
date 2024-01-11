c17e6757ab8522650333c3c3adc7fbae226f55fb
Jason Gaby <jasona.gaby@gmail.com>
2024-01-11 11:30:45 -0500

    Added beginner class files

---
+++ beginner/feed/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from sorl.thumbnail import ImageField
+
+class Post(models.Model):
+    text = models.CharField(max_length=140, null=False, blank=False)
+    image = ImageField()
+
+    def __str__(self):
+        return self.text
